499642ab6dfd3c23f2ece196d77c279d5b097b05
raingxm <raingxm@163.com>
2015-05-19 22:06:02 +0800

    a litte try to use freemarker

--- src/main/java/com/raingxm/mongo/HelloWorldSpark.java
+++ src/main/java/com/raingxm/mongo/HelloWorldSpark.java
@@ -1,17 +1,36 @@
 package com.raingxm.mongo;
 
-
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 import spark.Spark;
 
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class HelloWorldSpark {
+    private static Configuration configuration = new Configuration();
+
     public static void main(String[] args) {
+        configuration.setClassForTemplateLoading(HelloFreeMarker.class, "/");
+        StringWriter writer = new StringWriter();
         Spark.get(new Route("/") {
             @Override
             public Object handle(Request request, Response response) {
-                return "Hello World from spark";
+                try {
+                    Template helloTemplate = configuration.getTemplate("hello.ftl");
+                    Map<String, Object> helloMap = new HashMap<String, Object>();
+                    helloMap.put("name", "raingxm");
+                    helloTemplate.process(helloMap, writer);
+                } catch (Exception e) {
+                    halt(500);
+                    e.printStackTrace();
+                }
+                return writer;
             }
         });
     }
